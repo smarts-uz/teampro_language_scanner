9027048ff0f490a7dd15e33a49a04746b3c0be2c
Uktam19980416 <rakhmatullayev2019@mail.ru>
2021-02-21 23:43:41 +0500

    the latest version6

--- src/database/migrations/2021_02_17_175250_create_translations_table.php
+++ src/database/migrations/2021_02_17_175250_create_translations_table.php
@@ -14,8 +14,12 @@ class CreateTranslationsTable extends Migration
     public function up()
     {
         Schema::create('translations', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->integer('id', true);
+            $table->string('lang', 10)->nullable();
+            $table->text('lang_key')->nullable();
+            $table->text('lang_value')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
         });
     }
 
